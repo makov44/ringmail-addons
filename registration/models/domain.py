1cbe1ba684ebfa11902faaf7a591cf8452ed5544
Sergiy Makovskyy <smakovskyy@dyl.com>
2017-12-05 18:18:55 -0800

    Added domain verification.

--- registration/models/domain.py
+++ registration/models/domain.py
@@ -1,5 +1,11 @@
 from odoo import models, fields, api
+import random
+import base64
 
+def random_token():
+    # the token has an entropy of about 120 bits (6 bits/char * 20 chars)
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(20))
 
 class Domain(models.Model):
     _name = 'ringmail_reg.domain'
@@ -9,3 +15,20 @@ class Domain(models.Model):
         ('draft', 'Not Confirmed'),
         ('done', 'Confirmed'),
     ], string='Status', index=True, readonly=True, copy=False, default='draft')
+    txt_record = fields.Char(string='TXT record', readonly=True)
+    page = fields.Binary(string="HTML page", readonly=True)
+    page_fname = fields.Char(string="File name")
+
+    @api.multi
+    def confirm_domain(self):
+        self.state = 'done'
+
+
+    @api.model
+    def create(self, vals):
+        token = random_token()
+        vals['txt_record'] = 'ringmail-domain-verify='+token
+        vals['page_fname'] = 'ringmail_'+token+'.html'
+        vals['page'] = base64.encodebytes(str.encode(vals['txt_record']))
+        return super(Domain, self).create(vals)
+
